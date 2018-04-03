273fd486f7071fc1b704369aaea948ed0666c632
Carol Wai Ting Hung <carolhungwt@Carols-MacBook-Pro.local>
2018-04-02 23:10:12 -0400

    remove struct from readData.h

--- readData.h
+++ readData.h
@@ -1,5 +1,6 @@
 //readData.h
 #include "TMath.h"
+#include "TimeAnglePair.h"
 #include <istream>
 #include <fstream>
 #include <iterator>
@@ -7,18 +8,6 @@
 
 using namespace RooFit;
 using namespace std;
-
-struct datumpair
-{
-	double x;	//here x is delta time
-	double y;	//here y is delta angle in radian
-};
-
-std::istream& operator>>(std::istream& is, datumpair& datum){
-	is>>datum.x>>std::ws>>datum.y; // since we assume the datafile follow a format
-								   // x [whitespace] y
-	return is;
-}
 
 double arctan(double *x, double *par){
 	double xx = x[0];
